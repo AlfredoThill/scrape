03c696cb443c91ccee2262ca1742778b10965524
alfredo <alfredo.thill@dxc.com>
2024-08-02 17:06:11 -0600

    all

--- src/scrape.spec.ts
+++ src/scrape.spec.ts
@@ -5,7 +5,7 @@ test('test', async () => {
     downloadsPath: '/tmp',
   });
   const page = await browser.newPage();
-  test.setTimeout(120000); // bump run time if needed
+  test.setTimeout(3000000); // bump run time if needed
   await page.goto('http://sistemas1.buenosaires.edu.ar/wsad/frontend.php/');
   await page.locator('input[name="signin\\[username\\]"]').fill('CENS');
   await page.locator('input[name="signin\\[password\\]"]').fill('1234');
@@ -29,12 +29,18 @@ test('test', async () => {
   const options = await select?.$$eval('option', (options) =>
     options.map((option) => ({ value: option.value, text: option.textContent }))
   )!;
-  const cens = options.filter((option) => option?.text?.includes('CENS'));
+  console.log('all', options.length);
+  const notCens = options.filter((option) => option.text?.includes('CENS') === false);
+  console.log('notCens', notCens.length);
 
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < options.length; i++) {
     // cens.length
-    const { value, text } = cens[i];
-    if (value && text && text.includes('CENS')) {
+    if (options[i] === undefined) {
+      console.log('continue', i);
+      continue;
+    }
+    const { value, text } = options[i];
+    if (value && text) {
       if (i > 0) {
         await page.goto('http://sistemas1.buenosaires.edu.ar/wsad/frontend.php/designacion_responsable_acto_publico');
       }
@@ -56,7 +62,10 @@ test('test', async () => {
       const downloadPromise = page.waitForEvent('download');
       await page.locator('text=Exportar a Excel').click();
       const download = await downloadPromise;
-      await download.saveAs(`download-${i}.xlsx`);
+      await download.saveAs(`${text}.xls`);
+      console.log('download', text, i);
+    } else {
+      console.log('error', text, i);
     }
   }
 });
